6af58aa1983ab5fb1c097bded145272d248d1575
Alex Karacaoglu <alexkaracaoglu@Alexs-MacBook-Pro.local>
2019-01-27 11:43:45 -0500

    Section 5.5 - Enable/Disable and firstResponder

--- ToDoList/DetailViewController.swift
+++ ToDoList/DetailViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet weak var toDoField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
     
     var toDoItem: String?
 
@@ -19,12 +20,27 @@ class DetailViewController: UIViewController {
         if let toDoItem = toDoItem {
             toDoField.text = toDoItem
         }
+        toDoField.becomeFirstResponder()
+        enableDisableSaveButton()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "UnwindFromSave" {
             toDoItem = toDoField.text
         }
+    }
+    
+    func enableDisableSaveButton() {
+        if toDoField.text!.count > 0 {
+            saveBarButton.isEnabled = true
+        }
+        else {
+            saveBarButton.isEnabled = false
+        }
+    }
+    
+    @IBAction func toDoFieldChanged(_ sender: UITextField) {
+        enableDisableSaveButton()
     }
     
     @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
