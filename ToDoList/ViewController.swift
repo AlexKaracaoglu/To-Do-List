475530869aa6ea0ecbc3de661719811cebf25756
Alex Karacaoglu <alexkaracaoglu@Alexs-MacBook-Pro.local>
2019-01-27 16:37:22 -0500

    Section 5.6 - Add notes

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -18,6 +18,8 @@ class ViewController: UIViewController {
     
     var toDoArray = ["A", "B", "C", "D"]
     
+    var toDoNotesArray = ["a", "b", "c", "d"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
@@ -29,6 +31,7 @@ class ViewController: UIViewController {
             let destination = segue.destination as! DetailViewController
             let index = tableView.indexPathForSelectedRow!.row
             destination.toDoItem = toDoArray[index]
+            destination.toDoNoteItem = toDoNotesArray[index]
         }
         else {
             if let selectedPath = tableView.indexPathForSelectedRow {
@@ -40,21 +43,15 @@ class ViewController: UIViewController {
     @IBAction func unwindFromDetailViewController(segue: UIStoryboardSegue) {
         let sourceViewController = segue.source as! DetailViewController
         if let indexPath = tableView.indexPathForSelectedRow {
-            if sourceViewController.toDoItem! == "" {
-                toDoArray.remove(at: indexPath.row)
-                tableView.deleteRows(at: [indexPath], with: .fade)
-            }
-            else {
                 toDoArray[indexPath.row] = sourceViewController.toDoItem!
+                toDoNotesArray[indexPath.row] = sourceViewController.toDoNoteItem!
                 tableView.reloadRows(at: [indexPath], with: .automatic)
-            }
         }
         else {
             let newIndexPath = IndexPath(row: toDoArray.count, section: 0)
-            if sourceViewController.toDoItem != "" {
-                toDoArray.append(sourceViewController.toDoItem!)
-                tableView.insertRows(at: [newIndexPath], with: .automatic)
-            }
+            toDoArray.append(sourceViewController.toDoItem!)
+            toDoNotesArray.append(sourceViewController.toDoNoteItem!)
+            tableView.insertRows(at: [newIndexPath], with: .automatic)
         }
     }
     
@@ -81,20 +78,25 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         cell.textLabel?.text = toDoArray[indexPath.row]
+        cell.detailTextLabel?.text = toDoNotesArray[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             toDoArray.remove(at: indexPath.row)
+            toDoNotesArray.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
     
     func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         let itemToMove = toDoArray[sourceIndexPath.row]
+        let noteToMove = toDoNotesArray[sourceIndexPath.row]
         toDoArray.remove(at: sourceIndexPath.row)
         toDoArray.insert(itemToMove, at: destinationIndexPath.row)
+        toDoNotesArray.remove(at: sourceIndexPath.row)
+        toDoNotesArray.insert(noteToMove, at: destinationIndexPath.row)
     }
     
 }
